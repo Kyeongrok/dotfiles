443a674d931640958b5bb663b5ad1acbe3461993
Kyeongrok Kim <oceanfog1@gmail.com>
2025-02-14 17:20:09 +0900

    Update settings.js

--- cursorai/settings.js
+++ cursorai/settings.js
@@ -1,5 +1,13 @@
 {
     "editor.vim.enabled": true,
+    "vim.useSystemClipboard": true, 
+    "vim.leader": "<space>",
+    "vim.insertModeKeyBindings": [
+        {
+        "before": ["j", "j"],
+        "after": ["<Esc>"]
+        }
+    ],
     "vim.normalModeKeyBindings": [
         {
             "before": ["<S-h>"],
@@ -9,5 +17,20 @@
             "before": ["<S-l>"],
             "commands": ["workbench.action.nextEditor"]
         },
-    ]
+        {
+            "before": ["<A-h>"],
+            "commands": ["composer.showComposerChatHistory"]
+        },
+        {
+            "before": ["<leader>", "j"],
+            "commands": ["workbench.action.togglePanel"]
+        },
+        {
+            "before": ["<leader>", "e"],
+            "commands": [
+                "workbench.action.toggleSidebarVisibility",
+            ]
+        },
+    ],
+    "editor.fontSize": 18,
 }
