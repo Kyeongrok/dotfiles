332729bf166352b76e9a08222c1120d171ffd9c8
Kyeongrok Kim <oceanfog1@gmail.com>
2025-02-14 10:58:03 +0900

    Create keybindings.js

---
+++ cursorai/keybindings.js
@@ -0,0 +1,16 @@
+[
+    {
+        "key": "escape",
+        "command": "workbench.action.focusFirstEditorGroup",
+        "when": "!editorTextFocus"
+    },
+    {
+        "key": "alt+1",
+        "command": "workbench.view.explorer",
+        "when": "viewContainer.workbench.view.explorer.enabled"
+    },
+    {
+        "key": "ctrl+alt+l",
+        "command": "editor.action.formatDocument"
+    }
+]
