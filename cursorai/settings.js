f2db6dccf7d09a8aef493ca33bc26d60dc392be1
Kyeongrok Kim <oceanfog1@gmail.com>
2025-02-14 10:54:51 +0900

    Create settings.js

---
+++ cursorai/settings.js
@@ -0,0 +1,13 @@
+{
+    "editor.vim.enabled": true,
+    "vim.normalModeKeyBindings": [
+        {
+            "before": ["<S-h>"],
+            "commands": ["workbench.action.previousEditor"]
+        },
+        {
+            "before": ["<S-l>"],
+            "commands": ["workbench.action.nextEditor"]
+        },
+    ]
+}
